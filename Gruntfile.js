8ce5e7b83072dce731134a0b5d515cbb20530d1c
rb <rb2nem@gmail.com>
2017-11-05 12:00:59 +0100

    update

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,9 +4,6 @@ module.exports = function (grunt) {
 	// Project configuration.
 	grunt.initConfig({
 		pkg: grunt.file.readJSON('package.json'),
-                exec: {
-			uglify: 'uglifyjs tmp/renderer.browserified.js > tmp/bundle.js'
-		},
 		browserify: {
 			scripts: {
 				src: "src/renderer.js",
@@ -14,33 +11,22 @@ module.exports = function (grunt) {
 			}
 			
 		},
+                exec: {
+			uglify: 'uglifyjs tmp/renderer.browserified.js > tmp/renderer.uglified.js'
+		},
 		combine: {
 			single: {
 				input: "src/index.html",
 				output: "dist/offline_signer.html",
 				tokens: [
-					{ token: "//bundle.js", file: "./tmp/bundle.js" },
+					{ token: "//bundle.js", file: "./tmp/renderer.uglified.js" },
 				]
-			}
-		}
-
-,
-		
-		lineending: {               // Task
-			dist: {                   // Target
-				options: {              // Target options
-					eol: 'lf'
-				},
-				files: {                // Files to process
-					'./bitaddress.org.html': ['./bitaddress.org.html']
-				}
 			}
 		}
 	});
 
 	grunt.file.defaultEncoding = 'utf-8';
 	grunt.loadNpmTasks("grunt-combine");
-	grunt.loadNpmTasks('grunt-lineending');
         grunt.loadNpmTasks('grunt-browserify');
         grunt.loadNpmTasks('grunt-exec');
 	grunt.registerTask("default", ["browserify", "exec:uglify", "combine:single"]);
